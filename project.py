023be6b7da878824f219321b7faff11c79c8198d
AbuElzozz <120601753+AbuElzozz@users.noreply.github.com>
2022-12-15 18:09:53 +0200

    Update project.py

--- project.py
+++ project.py
@@ -94,26 +94,34 @@ def corp_page():
         username = stored_username
         corp_chosen = input("Hi " + username + " Do you want to (l)ist a job or (v)iew applicants?")
         if corp_chosen == "l":
-            xo = int(input("enter how many jobs you want to declare: "))
-            lp = 1
-            job = []
-            xp = 0
-            job_details = []
-            while xo >= lp:
-                print("job", lp)
-                job.append(str(input("enter job title: ")))
-                job_details.append(input("enter job description: "))
-                if xp == 0:
-                    with open("jobs.txt", "w") as f:
-                        f.write(job[xp] + ":" + "\n")
-                        f.write(job_details[xp] + "\n")
-                else:
-                    with open("jobs.txt", "a") as f:
-                        f.write(job[1] + ":" + "\n")
-                        f.write(job_details[1] + "\n")
-                f.close()
-                lp = lp + 1
-                xp = xp + 1
+            num = int(input("Enter number of jobs: "))
+            i = 1
+            j = 1
+            while (i<= num and j <= num) :
+                print("job",i)
+
+                title = input("Enter job title: ")
+                id = input("Enter job id: ")
+                required_skills =input ("Enter required skills: ")
+                job_describ =input("Enter job describtion: ")
+
+                f = open("Jobs.txt","w")
+                f.write("Title: "+ title +"\n")
+                f.write("id: " + id + "\n")
+                f.write("Required skills: " + required_skills + "\n")
+                f.write("Job describtion: " + job_describ + "\n\n")
+
+
+                fileName = title + ".txt"
+                x = open(fileName, "a")
+                x.write("Title: " + title + "\n")
+                x.write("id: " + id + "\n")
+                x.write("Required skills: " + required_skills + "\n")
+                x.write("Job describtion: " + job_describ + "\n\n")
+                j = j + 1
+                i = i + 1
+
+
         elif corp_chosen == "v":
             with open("job_applied.txt", "r")as f:
                 job_applied = f.read()
@@ -121,7 +129,7 @@ def corp_page():
             with open("applicant_description.txt", "r")as f:
                 applicant_description = f.read()
                 f.close()
-            with open(username+".txt", "r") as f:
+            with open(stored_fren_username+".txt", "r") as f:
                 stored_username = f.read().split("\n")
                 f.close()
             print(stored_username[0] + " has applied to " + job_applied)
@@ -145,6 +153,7 @@ def corp_page():
 def freelancer_page():
     zero = 0
     global stored_fren_username
+    global job_applied
     with open(f_username+".txt", "r") as f:
         stored_fren_username, stored_password = f.read().split("\n")
         f.close()
@@ -152,17 +161,11 @@ def freelancer_page():
         print("Hi " + username + " there are some jobs available. Do you want to see them?\n")
         viewjobs = input("(y)es, or (n)o\n")
         if viewjobs == "y":
-            with open("jobs.txt", "r") as f:
-                job1 = f.read().split("\n")
-                job1_details = f.read().split("\n")
+            search=input("Please Enter Title Job : \n")
+            job_applied=search
+            with open(job_applied+".txt", "r") as f:
+                print(f.read())
                 f.close()
-            check_numberline=len(job1)
-            lines=0
-            while lines<=check_numberline:
-                print(job1[lines]+"\n"+job1[lines+1]+"\n")
-                lines+=2
-                break
-                #we need to put limit for range
         pass
         job_apply = input("Do you want to apply for a (j)ob? or do you want to see job (s)tatus?" + "\n")
         while job_apply == "j":
@@ -170,26 +173,8 @@ def freelancer_page():
             with open("applicant_description.txt", "w")as f:
                 f.write(applicant_description + "\n")
                 f.close()
-            i = int(input("Please enter job's number " + "\n"))
-            if i == 1:
                 with open("job_applied.txt", "w") as f:
-                    f.write(job1[0])
-                    f.close()
-            elif i == 2:
-                with open("job_applied.txt", "w") as f:
-                    f.write(job1[2])
-                    f.close()
-            elif i == 3:
-                with open("job_applied.txt", "w") as f:
-                    f.write(job1[4])
-                    f.close()
-            elif i == 4:
-                with open("job_applied.txt", "w") as f:
-                    f.write(job1[6])
-                    f.close()
-            elif i == 5:
-                with open("job_applied.txt", "w") as f:
-                    f.write(job1[8])
+                    f.write(job_applied)
                     f.close()
             print("Job request sent successfully !")
             break
